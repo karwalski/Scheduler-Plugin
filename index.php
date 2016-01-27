6ad81420b19bb1d955d31d43630aa3f9363f9c05
Matt Watt <matt@karwalski.net>
2016-01-28 10:28:40 +1100

    Touchfile

---
+++ index.php
@@ -0,0 +1,26 @@
+<?PHP
+
+
+// Scheduler plugin for WP
+
+?>
+<HTML>
+<HEAD>
+<TITLE>
+Scheduler workspace
+</TITLE>
+</HEAD>
+<BODY>
+<DIV NAME="CONTENT" STYLE="min-height:100%;margin-bottom:-100px;">
+<H1> Scheduler Plugin for WP - workspace</H1>
+<BR />
+<BR />
+
+
+</DIV>
+<DIV NAME="FOOTER" STYLE="height:100px;background:#000;color:#FFF;text-align:center;">Matt Watt 2016 - GPLv3 copyleft</DIV>
+
+
+
+</BODY>
+</HTML>
