138ee38b54de84689cb441e2cde98718e6a70aea
Matt Watt <matt@karwalski.net>
2016-01-28 13:22:15 +1100

    convert to php

--- db.php
+++ db.php
@@ -1,4 +1,18 @@
-CREATE TABLE IF NOT EXISTS `scheduler`.`parent` (
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+$sql = "CREATE TABLE IF NOT EXISTS `scheduler`.`parent` (
   `id` INT NOT NULL,
   `username` VARCHAR(45) NULL,
   `password` VARCHAR(45) NULL,
@@ -16,10 +30,16 @@ CREATE TABLE IF NOT EXISTS `scheduler`.`parent` (
   `alternatephone` VARCHAR(45) NULL,
   `datejoined` DATE NULL,
   PRIMARY KEY (`id`))
-;
+  ";
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "Table parent created successfully";
+} else {
+    echo "Error creating table parent: " . $conn->error;
+}
 
 
-CREATE TABLE IF NOT EXISTS `scheduler`.`child` (
+$sql = "CREATE TABLE IF NOT EXISTS `scheduler`.`child` (
   `id` INT NOT NULL,
   `parentid` INT NULL,
   `firstname` VARCHAR(45) NULL,
@@ -32,28 +52,46 @@ CREATE TABLE IF NOT EXISTS `scheduler`.`child` (
   `other` VARCHAR(45) NULL,
   `rate` DECIMAL(4,2) NULL,
   PRIMARY KEY (`id`))
-;
+  ";
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "Table child created successfully";
+} else {
+    echo "Error creating table child: " . $conn->error;
+}
 
 
-CREATE TABLE IF NOT EXISTS `scheduler`.`booking` (
+$sql = "CREATE TABLE IF NOT EXISTS `scheduler`.`booking` (
   `id` INT NOT NULL,
   `childid` INT NULL,
   `starttime` DECIMAL(4,2) NULL,
   `endtime` DECIMAL(4,2) NULL,
   PRIMARY KEY (`id`))
-;
+  ";
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "Table booking created successfully";
+} else {
+    echo "Error creating table booking: " . $conn->error;
+}
 
-CREATE TABLE IF NOT EXISTS `scheduler`.`admin` (
+$sql = "CREATE TABLE IF NOT EXISTS `scheduler`.`admin` (
   `id` INT NOT NULL,
   `starttime` DECIMAL(4,2) NULL,
   `endtime` DECIMAL(4,2) NULL,
   `buffertime` DECIMAL(4,2) NULL,
   `timezone` VARCHAR(45) NULL,
   PRIMARY KEY (`id`))
-;
+  ";
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "Table admin created successfully";
+} else {
+    echo "Error creating table admin: " . $conn->error;
+}
 
 
-CREATE TABLE IF NOT EXISTS `scheduler`.`staff` (
+$sql = "CREATE TABLE IF NOT EXISTS `scheduler`.`staff` (
   `id` INT NOT NULL,
   `username` VARCHAR(45) NULL,
   `password` VARCHAR(45) NULL,
@@ -61,13 +99,28 @@ CREATE TABLE IF NOT EXISTS `scheduler`.`staff` (
   `lastname` VARCHAR(45) NULL,
   `adminlevel` INT NULL,
   PRIMARY KEY (`id`))
-;
+  ";
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "Table staff created successfully";
+} else {
+    echo "Error creating table staff: " . $conn->error;
+}
 
 
-CREATE TABLE IF NOT EXISTS `scheduler`.`available` (
+$sql = "CREATE TABLE IF NOT EXISTS `scheduler`.`available` (
   `id` INT NOT NULL,
   `staffid` INT NULL,
   `starttime` DECIMAL(4,2) NULL,
   `endtime` DECIMAL(4,2) NULL,
   PRIMARY KEY (`id`)
-;
+  ";
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "Table available created successfully";
+} else {
+    echo "Error creating table available: " . $conn->error;
+}
+
+
+?>
